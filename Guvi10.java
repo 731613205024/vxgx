b4f8da0a9189f73cbf73b0d7e7ee17ac92824b83
731613205024 <731613205024@users.noreply.github.com>
2016-09-28 13:47:31 +0530

    Add files via upload

--- Guvi10.java
+++ Guvi10.java
@@ -4,16 +4,31 @@
  */
 package guvi10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Student
  */
 public class Guvi10 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static int dot(int upper,int lower){
+	int sum=0;
+int temp=0;
+for(int i=1;i<upper;i++)
+	temp+=i;
+for(int j=1;j<lower;j++)
+	sum+=temp*j;
+return sum;
     }
+    public static void main(String[] args) 
+    {
+      Scanner scr=new Scanner(System.in);
+System.out.println("Enter number of dots at upper row");
+int upper=scr.nextInt();
+System.out.println("Enter the number of dots at lower row");
+int lower=scr.nextInt();
+System.out.println(dot(upper,lower));
 }
+    }
+
